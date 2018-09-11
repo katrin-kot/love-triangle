86a2be6144f971a2f05a38a1e9d95381dfb61ae5
katrin-kot <enkor89@mail.ru>
2018-09-11 22:14:02 +0300

    done

--- src/index.js
+++ src/index.js
@@ -3,5 +3,14 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  let count=0;
+  for (var i = 0; i < preferences.length; i++) {
+   const first =preferences[i];
+  const second=preferences[first-1];
+  const third=preferences[second-1];
+  if (third-1===i && first !== second) {
+    count=count+1;
+  }
+  };
+  return count/3;
 };
